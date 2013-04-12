84542219ee88bb8c459c688b9ca8c08806d07af4
pzhifeng <pzhifeng@gmail.com>
2013-04-12 18:06:45 +0800

    VoObject.h位置

---
+++ client-main/Classes/core/SenceUI.cpp
@@ -0,0 +1,9 @@
+//
+//  SenceUI.cpp
+//  client-main
+//
+//  Created by PAN ZHIFENG on 13-4-12.
+//
+//
+
+#include "SenceUI.h"
---
+++ client-main/Classes/core/SenceUI.h
@@ -0,0 +1,14 @@
+//
+//  SenceUI.h
+//  client-main
+//
+//  Created by PAN ZHIFENG on 13-4-12.
+//
+//
+
+#ifndef __client_main__SenceUI__
+#define __client_main__SenceUI__
+
+#include <iostream>
+
+#endif /* defined(__client_main__SenceUI__) */
