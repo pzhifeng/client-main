1e726e44d5bd1e9f7ffe5dd942608e9135272065
liuzhuang <11660629@qq.com>
2013-04-18 15:20:51 +0800

    modified

--- client-main/Classes/core/SmartRes.cpp
+++ client-main/Classes/core/SmartRes.cpp
@@ -27,30 +27,29 @@ bool SmartRes::init(void)
 void SmartRes::setDesignResolutionSize()
 {
     // 木有找到 2.0 里面怎么获取设备方向。。所以假定是横向了
+	CCDirector* pDirector = CCDirector::sharedDirector();
+	CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
 	//得到屏幕的真实分辨率大小
-	CCSize frameSize= CCEGLView::sharedOpenGLView()->getFrameSize();
+	CCSize frameSize= pEGLView->getFrameSize();
 	CCLOG("winSize width: %0.2f, winSize height: %0.2f",frameSize.width, frameSize.height);
 
     m_scale = frameSize.width / resSize.width;
     designSize.width = resSize.width;
     designSize.height = frameSize.height / m_scale;
 	//设置设计分辨率大小   
-    CCEGLView::sharedOpenGLView()->setDesignResolutionSize(designSize.width, designSize.height, kResolutionNoBorder);
+    pEGLView->setDesignResolutionSize(designSize.width, designSize.height, kResolutionNoBorder);
+	//资源背景的高度适应设计分辨率
+	pDirector->setContentScaleFactor(resSize.height/designSize.height);
 
     CCLOG("Virtual width: %0.2f, Virtual height: %0.2f, scale: %0.2f",designSize.width, designSize.height, m_scale);
 
-    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    CCSize winSize = pDirector->getWinSize();
     m_center.x = winSize.width / 2;
     m_center.y = winSize.height / 2;
     m_left = 0;
     m_right = designSize.width - 1;
-	if(designSize.height<resSize.height){
-		m_top = designSize.height - 1;
-		m_bottom = 0;
-	}else{
-		m_top = (designSize.height-resSize.height)/2+resSize.height - 1;
-		m_bottom = (designSize.height-resSize.height)/2-1;
-	}
+	m_top = designSize.height - 1;
+	m_bottom = 0;
     CCLOG("center x: %0.2f, center y: %0.2f", m_center.x, m_center.y);
     CCLOG("top: %0.2f, right: %0.2f, bottom: %0.2f, left: %0.2f", m_top, m_right, m_bottom, m_left);
 }
